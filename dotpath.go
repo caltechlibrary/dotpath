14dfe37b51db20560da1e34ad682f46c20d72657
R. S. Doiel <=>
2018-05-20 10:43:30 -0700

    added ToLabel(), bumpted version no

--- dotpath.go
+++ dotpath.go
@@ -27,7 +27,7 @@ import (
 	"strings"
 )
 
-const Version = `v0.0.1`
+const Version = `v0.0.2`
 
 // JSONDecode decodes JSON using the json.Number instead of float64 for numeric values.
 func JSONDecode(buf []byte) (interface{}, error) {
